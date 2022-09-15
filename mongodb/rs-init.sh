41a3363b92f8c0818c4e03b21e8ac878ac8edbbe
thienthn <thienthn@gglive.vn>
2022-09-15 10:45:46 +0700

    fix: add mongo replicaset

---
+++ mongodb/rs-init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DELAY=25
+
+mongo <<EOF
+var config = {
+    "_id": "dbrs",
+    "version": 1,
+    "members": [
+        {
+            "_id": 1,
+            "host": "127.0.0.1:27017",
+            "priority": 2
+        }
+    ]
+};
+rs.initiate(config, { force: true });
+EOF
+
+echo "****** Waiting for ${DELAY} seconds for replicaset configuration to be applied ******"
+
+sleep $DELAY
+
+mongo < /scripts/init.js
+
+# ,
+  #        {
+  #            "_id": 2,
+  #            "host": "mongo2:27017",
+  #            "priority": 1
+  #        },
+  #        {
+  #            "_id": 3,
+  #            "host": "mongo3:27017",
+  #            "priority": 1
+  #        }
